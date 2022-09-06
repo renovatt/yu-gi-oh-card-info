ba45ce411d9ecd6e9eae3089892264127843162f
Will <wlymes@gmail.com>
2022-09-06 13:02:54 -0300

    sound function added and position fixed

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -17,8 +17,19 @@ const list_name = document.querySelector('#list-select-name')
 const divine_beast = document.querySelector('#list-select-divine')
 const preview = document.querySelector('#preview')
 
+//Atribuição dos valores relacionado aos sons
+const easter_egg = document.querySelector('#easter-egg')
+const container = document.querySelector('#container')
+const pause = document.querySelector('#pause')
+
+//Atribuição dos sons
+const flip_sound = new Audio('./assets/audio/card-flip.mp3')
+const easter_sound= new Audio('./assets/audio/easter-sound.mp3')
+const theme_sound = new Audio('./assets/audio/theme-sound.mp3')
+
 let card_indice = 10725
 let new_input_value
+let new_indice_value
 //2398 posição do 'Dark Magician'
 //=========================================================================
 
@@ -95,7 +106,7 @@ const nameRender = async () => {
             img_preview.src = `${search.card_images[0].image_url}`
             card_set.innerHTML = `${search.card_sets[0].set_name}`
             race.innerHTML = `${search.race}`
-            position.innerHTML = `${card_indice}`
+            position.innerHTML = `${new_indice_value}`
             total_cards.innerHTML = `${data.data.length}`
         } 
     }
@@ -105,8 +116,8 @@ const nameRender = async () => {
 const nameList = async () => {
     const data = await fetchYuGiOh()
     const filter = data.data
-    const listName = filter.map(item =>    
-    `<li onclick="catchNames(event)">${item.name}</li>`)
+    const listName = filter.map((item, key) =>    
+    `<li data-id="${key}" onclick="catchNames(event)">${item.name}</li>`)
     list_of_names.innerHTML = listName.join('')
 }
 
@@ -116,8 +127,8 @@ nameList()
 const easterEgg = async () => {
     const data = await fetchDivineBeast()
     const filter = data.data
-    const listName = filter.map(item =>    
-    `<li onclick="catchNames(event)">${item.name}</li>`)
+    const listName = filter.map((item, key) =>    
+    `<li data-id="${key}" onclick="catchNames(event)">${item.name}</li>`)
     divine_of_beast.innerHTML = listName.join('')
 }
 
@@ -132,8 +143,9 @@ function catchNames(event){
     new_input_value = event.target.textContent
     list_name.style.display = 'none'
     divine_beast.style.display = 'none'
-    preview.style.visibility = 'visible' 
+    preview.style.visibility = 'visible'
     nameRender()
+    soundFlip()
 }
 
 //Capturando o valor do input digitado e tratando erro de espaços
@@ -142,13 +154,15 @@ function btnSubmit(){
     new_input_value = input.value.replace(/( )+/g, ' ').trim()
         input.value = ''
         nameRender()
+        soundFlip()
 }
 
 //Selecionando e buscando os dados das próximas cartas
 function btnNext() {
     if(card_indice < 12144){
         card_indice += 1 
-    globalRender()
+        globalRender()
+        soundFlip()
     }
 }
 
@@ -157,6 +171,7 @@ function btnPrev() {
     if(card_indice > 0){
         card_indice -= 1
         globalRender()
+        soundFlip()
     }
 }
 
@@ -180,10 +195,44 @@ function closeLists(){
     list_name.style.display = 'none'
     preview.style.visibility = 'visible'
 }
+
+//Som de flip efeito
+function soundFlip() {
+    setTimeout(sound, 500)
+    function sound() {
+        flip_sound.play()
+    }
+}
+
 //=========================================================================
 
 //Evento de Preload
 window.addEventListener('load', function(){
     const overlay = document.querySelector('#overlay')
     overlay.style.display = 'none'
+})
+
+//Pegando o indice de cada item da lista e jogando na posição
+list_of_names.addEventListener('click', function (event) {
+    new_indice_value = +event.target.dataset.id+1
+})
+
+//Pegando o indice de cada item da lista easter-egg e jogando na posição
+divine_of_beast.addEventListener('click', function (event) {
+    new_indice_value = +event.target.dataset.id+1
+})
+
+//Easter-egg som efeito
+easter_egg.addEventListener('mouseover', function () {
+    easter_sound.play()
+    theme_sound.play()
+    easter_sound.volume = 0.8
+    theme_sound.volume = 0.1
+    pause.style.display = "flex"
+})
+
+//Pausando a musica do easter-egg
+pause.addEventListener('click', function () {
+    theme_sound.pause()
+    pause.style.display = "none"
 })
